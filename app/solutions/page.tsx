f740d178bde8391f89f7f286bb41a7f3b19a8ed3
unknown <giolo.evora@gmail.com>
2025-04-15 09:01:17 +0800

    update

---
+++ app/solutions/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { poetsen_one } from "@/config/fonts";
+import Cards from "@/components/user/home/solutions/cards";
+
+const Page = () => {
+  return (
+    <section className="container mx-auto py-12 px-4">
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-between">
+          <div className="max-w-lg text-center">
+            <h1 className="text-4xl text-accent font-bold">SOLUTIONS</h1>
+            <h1 className={`text-3xl text-primary ${poetsen_one.className}`}>
+              We design & build your custom website
+            </h1>
+          </div>
+        </div>
+
+        <div>
+          <Cards />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
